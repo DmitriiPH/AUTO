ba2be7c6451a96a52ecd6503f28f55bd1e7196aa
Dimka <alpatovph@gmail.com>
2025-08-22 06:43:37 +0300

    prepare to test

--- for test/l8.py
+++ for test/l8.py
@@ -1,7 +1,5 @@
-def gen(t1,t2):
-    return f'Text one two trip {t1} and {t2}'
+#def gen(t1,t2):
+#    return f'Text one two trip {t1} and {t2}'
 
-print(gen('qwe','tre'))
-
-
+#print(gen('qwe','tre'))
 
